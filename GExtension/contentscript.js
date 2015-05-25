d7af6eadd2f4cb4d80510a88cbefff12cdd1fc1b
luisim0 Desktop <sun.elmim@gmail.com>
2015-05-25 14:16:10 -0500

    Basefiles

--- GExtension/contentscript.js
+++ GExtension/contentscript.js
@@ -2,7 +2,7 @@ function build_menu(){
 	var menu = document.createElement('div');
 	menu.id = 'fapp_main_panel';
 	menu.className = 'fapp_main_panel';
-	menu.innerHTML = "<div id='fapp_frame_manipulate'><div id='fapp_hide_holder'><img src='' id='fapp_hide_button'/></div><div id='fapp_logo_holder'><img src='' id='fapp_logo'/></div><div id='fapp_frame_contents'></div></div>";
+	menu.innerHTML = "<div id='fapp_frame_manipulate'><div id='fapp_hide_holder'><img src='' id='fapp_hide_button'/></div><div id='fapp_logo_holder'><img src='' id='fapp_logo'/></div></div><div id='fapp_frame_contents'></div>";
 	
 	//Insert structure
 	document.body.appendChild(menu);
