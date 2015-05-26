d6afd7256da2b14d178f34f623f5d29653afced6
luisim0 DesktopVAIO <sun.elmim@gmail.com>
2015-05-26 17:43:43 -0500

    That's all folks!
    Filling data divs... REMEMBER TO MAKE CATEGORIES TO CHANGE COLOR WHEN
    PRESSED!

--- GExtension/contentscript.js
+++ GExtension/contentscript.js
@@ -5,11 +5,13 @@ var status_ready = chrome.extension.getURL("IMGs/ready.png");
 var status_loading = chrome.extension.getURL("IMGs/loading.gif");
 var status_wrong = chrome.extension.getURL("IMGs/wrong.png");
 var fapp_logo = chrome.extension.getURL("IMGs/facturapp_logo_ver.png");
+var office_generic = chrome.extension.getURL("IMGs/office.png");
+
 //Functions
 function build_menu(){
 	var menu = document.createElement('div');
 	menu.id = 'fapp_main_panel';
-	menu.innerHTML = "<div id='fapp_frame_manipulate'><div id='fapp_hide_holder'><img src='' id='fapp_hide_button' class='no_rotate' width='40' height='40'></div><div id='fapp_status_holder'><img src='' id='fapp_status' height='30' width='30'></div><div id='fapp_logo_holder'><img src='' id='fapp_logo'></div></div><div id='fapp_frame_contents' class='fapp_frame_contents'><div id='fapp_contents_heading'><p>Menú Principal</p></div><div id='fapp_cat_0' class='fapp_category_container'><p>Perfil</p></div><div class='fapp_data_container'></div><div id='fapp_cat_1' class='fapp_category_container'><p>Registro de RFCs</p></div><div class='fapp_data_container'><a id='test'>Place RFC</a></div><div id='fapp_cat_2' class='fapp_category_container'><p>Descargar</p></div><div class='fapp_data_container_populated'></div></div>";
+	menu.innerHTML = "<div id='fapp_main_panel'><div id='fapp_frame_manipulate'><div id='fapp_hide_holder'><img src='' id='fapp_hide_button' class='no_rotate' width='40' height='40'></div><div id='fapp_status_holder'><img src='' id='fapp_status' height='30' width='30'></div><div id='fapp_logo_holder'><img src='' id='fapp_logo'></div></div><div id='fapp_frame_contents' class='fapp_frame_contents'><div id='fapp_contents_heading'><p>Menú Principal</p></div><div id='fapp_cat_0' class='fapp_category_container'><p>Perfil</p></div><div class='fapp_data_container'><div id='fapp_profile_basics'><div class='fapp_office_icon'><img src='' id='fapp_office_logo' width='35' height='35'></div><div class='fapp_office_data'><div class='fapp_office_name'>COMPANY NAME S.A. de C.V.</div><div class='fapp_office_RFC'>UTE150219H68</div><div class='fapp_office_offices'><a id=''>1</a> Empresa</div></div></div></div><div id='fapp_cat_1' class='fapp_category_container'><p>Registro de RFCs</p></div><div class='fapp_data_container'><a id='test'>Place RFC</a></div><div id='fapp_cat_2' class='fapp_category_container'><p>Descargar</p></div><div class='fapp_data_container_populated'></div></div></div>";
 	
 	//Insert structure
 	document.body.appendChild(menu);
@@ -20,6 +22,7 @@ function build_menu(){
 	var status_image = document.getElementById("fapp_status");
 	status_image.src = status_ready;
 	document.getElementById("fapp_logo").src = fapp_logo;
+	document.getElementById("fapp_office_logo").src = office_generic;
 }
 
 function add_listeners(){
