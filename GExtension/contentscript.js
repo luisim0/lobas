1e4137fd34f1abbc8ef9bb71c3f9bfa2156751ec
luisim0 DesktopVAIO <sun.elmim@gmail.com>
2015-05-26 16:29:58 -0500

    Everything is awesome! jeje
    The interface has been improved by adding animations for category
    selection.
    Page re-directioning has also been addressed; however, it must be
    further improved by a state-machine.

--- GExtension/contentscript.js
+++ GExtension/contentscript.js
@@ -9,7 +9,7 @@ var status_wrong = chrome.extension.getURL("IMGs/wrong.png");
 function build_menu(){
 	var menu = document.createElement('div');
 	menu.id = 'fapp_main_panel';
-	menu.innerHTML = "<div id='fapp_frame_manipulate'><div id='fapp_hide_holder'><img src='' id='fapp_hide_button' class='no_rotate'/></div><div id='fapp_status_holder'><img src='' id='fapp_status'/></div><div id='fapp_logo_holder'><img src='' id='fapp_logo'/></div></div><div id='fapp_frame_contents' class='fapp_frame_contents'><a id='test'>Place RFC</a></div>";
+	menu.innerHTML = "<div id='fapp_frame_manipulate'><div id='fapp_hide_holder'><img src='' id='fapp_hide_button' class='no_rotate'/></div><div id='fapp_status_holder'><img src='' id='fapp_status'/></div><div id='fapp_logo_holder'><img src='' id='fapp_logo'/></div></div><div id='fapp_frame_contents' class='fapp_frame_contents'></div>";
 	
 	//Insert structure
 	document.body.appendChild(menu);
@@ -20,7 +20,7 @@ function build_menu(){
 	hide_button.width = "40"; hide_button.height = "40";
 	
 	var status_image = document.getElementById("fapp_status");
-	status_image.src = chrome.extension.getURL("IMGs/ready.png");
+	status_image.src = status_ready;
 	status_image.height = "30";
 	status_image.width = "30";
 	
@@ -34,29 +34,32 @@ function build_menu(){
 	
 	//Profile management
 	menu = document.createElement("div");
+	menu.id = "fapp_cat_0";
 	menu.className = "fapp_category_container";
 	menu.innerHTML = "<p>Información de Cuenta</p>";
 	document.getElementById("fapp_frame_contents").appendChild(menu);
 	//Contents div
 	menu = document.createElement("div");
 	menu.className = "fapp_data_container";
-	menu.innerHTML = "<p>Something</p><br><p>Something</p><br><p>Something</p><br><p>Something</p><br><p>Something</p><br><p>Something</p><br><p>Something</p><br><p>Something</p><br><p>Something</p><br><p>Something</p><br><p>Something</p>";
 	document.getElementById("fapp_frame_contents").appendChild(menu);
 	
 	//RFC Registry
 	menu = document.createElement("div");
+	menu.id = "fapp_cat_1";
 	menu.className = "fapp_category_container";
 	menu.innerHTML = "<p>Registro de RFCs</p>";
 	document.getElementById("fapp_frame_contents").appendChild(menu);
 	//Contents div
 	menu = document.createElement("div");
 	menu.className = "fapp_data_container";
+	menu.innerHTML = "<a id='test'>Place RFC</a>";
 	document.getElementById("fapp_frame_contents").appendChild(menu);
 	
 	//Explorer Settings
 	menu = document.createElement("div");
+	menu.id = "fapp_cat_2";
 	menu.className = "fapp_category_container";
-	menu.innerHTML = "<p>Nueva búsqueda</p>";
+	menu.innerHTML = "<p>Descargar</p>";
 	document.getElementById("fapp_frame_contents").appendChild(menu);
 	//Contents div
 	menu = document.createElement("div");
@@ -64,7 +67,7 @@ function build_menu(){
 	document.getElementById("fapp_frame_contents").appendChild(menu);
 }
 
-function add_hide_listener(){
+function add_listeners(){
 	//Button to hide/show the sidepanel
 	var hide_button = document.getElementById("fapp_hide_button");
 	hide_button.addEventListener('click',function(){
@@ -83,6 +86,19 @@ function add_hide_listener(){
 	hide_button.addEventListener('mouseout',function(){
 		hide_button.src = hide_unnactive_URL;
 	});
+	
+	//Category selectors
+	var cat_links = document.getElementsByClassName("fapp_category_container");
+	var n = cat_links.length;
+	for(i = 0; i < n; i++){
+		cat_links[i].addEventListener('click',function(){
+			var i = parseInt(this.id.split("_")[2]);
+			var cat_links = document.getElementsByClassName(this.className);
+			for(j = 0; j < n; j++){
+				(j != i) ? cat_links[j].nextSibling.className = "fapp_data_container" : cat_links[j].nextSibling.className = "fapp_data_container_populated"; 
+			}
+		});
+	}
 	
 	//This is a php message test
 	var test_link = document.getElementById("test");
@@ -103,6 +119,31 @@ function add_hide_listener(){
 	});
 }
 
+function check_page(){
+	if(window.location.href.indexOf("https://cfdiau.sat.gob.mx/nidp/app?") == 0){
+		//We reached the netiq access manager... :(
+		window.location.replace("https://portalcfdi.facturaelectronica.sat.gob.mx/");
+		return false;
+	}else if(window.location.href.indexOf("https://cfdiau.sat.gob.mx/nidp/lofc.jsp") == 0){
+		//Ended session
+		window.location.replace("https://cfdiau.sat.gob.mx/nidp/app/login?id=SATUPCFDiCon&sid=0&option=credential&sid=0");
+		return false;
+	}/*else{
+		var ind = document.getElementsByClassName("contrasena")[0].getElementsByTagName("a")[0];
+		if(ind.innerHTML == "Fiel"){//Good page
+			return true;
+		}else{//Bad page
+			return false;
+		}
+	}*/
+	return true;
+}
+
 //Main!
-build_menu();
-add_hide_listener();
+if(check_page()){
+	build_menu();
+	add_listeners();
+}/*else{
+	var ind = document.getElementsByClassName("contrasena")[0].getElementsByTagName("a")[0];
+	ind.click();
+}*/
