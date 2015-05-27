b5dc30c0affad42e5da1b08afe6cc99ce126ab98
luisim0 DesktopVAIO <sun.elmim@gmail.com>
2015-05-27 17:25:50 -0500

    I'll continue later
    Almost ready...

--- GExtension/popupm.js
+++ GExtension/popupm.js
@@ -21,6 +21,9 @@ document.addEventListener('DOMContentLoaded', function() {
   		var form = document.getElementById("fapp_ext_form");
   		form.submit();
   	});
+  	document.getElementById("fapp_ext_signin").addEventListener('click',function(){
+  		alert("Actualmente en desarrollo   ;)");
+  	});
   	document.getElementById("fapp_ext_pss").addEventListener('keypress',function(e){
   		if(e.keyCode == 13){
   			var form = document.getElementById("fapp_ext_form");
@@ -29,4 +32,25 @@ document.addEventListener('DOMContentLoaded', function() {
   	});
   	//Initialize
   	main();
+});
+
+var jsoned = JSON.parse('{"action":"get_php","method":"GET","url":"http://facturapp.eu.pn/PHP/isLogged.php","data":[]}');
+chrome.extension.sendMessage(jsoned,function(response){
+	if(response.answer == 'Error'){
+		alert("Los servidores de Facturapp están temporalmente fuera de servicio. Por favor intente más tarde.");
+		//Guardar en chrome.storage no session
+	}else{
+		var yesno = response.answer;
+		if(yesno == "0"){//There is no previous session
+			//Guardar en chrome.storage no session
+			//Load login page
+			//Remember that the event handler must depend on which paga was loaded
+			alert("Must load login page");
+		}else{//There is already a session
+			//Guardar en chrome.storage sí session
+			//Load link page
+			//Remember that the event handler must depend on which paga was loaded
+			alert("Must load link page");
+		}
+	}
 });
