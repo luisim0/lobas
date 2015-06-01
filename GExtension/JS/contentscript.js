9b936c1180e92ab7b4aa20f12163228deca4668c
luisim0 DesktopVAIO <sun.elmim@gmail.com>
2015-06-01 18:10:05 -0500

    Search bar done!!

--- GExtension/JS/contentscript.js
+++ GExtension/JS/contentscript.js
@@ -1,7 +1,7 @@
 //SAT URLs
 var iqaccess_url = "https://cfdiau.sat.gob.mx/nidp/app?";
-var weird_sat_login = "https://cfdiau.sat.gob.mx/nidp/lofc.jsp";
-var valid_sat_login = "https://cfdiau.sat.gob.mx/nidp/app/login?id=SATUPCFDiCon&sid=0&option=credential&sid=0";
+var weird_sat_login = "https://cfdiau.sat.gob.mx/nidp/lofc.jsp";//Cuando termina la sesión
+var valid_sat_login = "https://cfdiau.sat.gob.mx/nidp/app/login?id=SATUPCFDiCon";
 var logged_sat_url = "https://portalcfdi.facturaelectronica.sat.gob.mx/";
 var valid_sat_token = "https://cfdiau.sat.gob.mx/nidp/app/login?id=SATUPCFDiCon";
 
@@ -16,6 +16,8 @@ var status_wrong = chrome.extension.getURL("IMGs/wrong.png");
 var fapp_logo = chrome.extension.getURL("IMGs/facturapp_logo_ver.png");
 var office_generic = chrome.extension.getURL("IMGs/edificio.png");
 var json_arr;
+var prev_search = "";
+var sel_index = 0;
 
 //Functions
 function build_menu(){
@@ -60,7 +62,7 @@ function build_menu(){
 						var client_data = document.getElementById("fdc1");
 						for(i = 0;i < n;i++){
 							var par_div = document.createElement("div");
-							par_div.className = "fapp_client_box"; par_div.id = json_arr[i].id;
+							par_div.className = "fapp_client_box fapp_client_normal"; par_div.id = json_arr[i].id;
 							par_div.innerHTML = response.answer.replace(/[\r\n\t]/g, "");
 							par_div.getElementsByClassName("fapp_client_name_holder")[0].innerHTML = json_arr[i].name;
 							par_div.getElementsByClassName("fapp_client_RFC")[0].innerHTML = json_arr[i].rfc;
@@ -72,10 +74,25 @@ function build_menu(){
 				});
 			}
 		});
-		
-		//Add Listeners, this is performed inside the call for clients
 	};
 	get_side_bar.send();
+}
+
+function get_search_results(){
+	var clients = document.getElementById("fdc1").children;var n = clients.length;
+	var selected = [];
+	for(i = 0;i < n;i++){
+		var class_parts = clients[i].className.split(" ");
+		if(class_parts[1] != "fapp_client_hidden"){
+			selected.push(clients[i]);
+		}
+	}
+	return {selected:selected, n:selected.length, class_parts:selected[sel_index].className.split(" ")};
+}
+
+function set_selected_num(){
+	document.getElementById("fapp_client_count").innerHTML = document.getElementsByClassName("fapp_client_selected").length;
+	return true;
 }
 
 function add_listeners(){
@@ -92,7 +109,7 @@ function add_listeners(){
 		}
 	});
 	
-	//Link #of clients
+	//Link #of clients (the number in the )
 	document.getElementById("fapp_num_clients").addEventListener('click',function(){
 		document.getElementById("fapp_cat_1").click();
 	});
@@ -129,19 +146,91 @@ function add_listeners(){
 	cat_links = document.getElementsByClassName("fapp_client_icon"); n = cat_links.length;
 	for(i = 0;i < n;i++){
 		cat_links[i].addEventListener('click',function(){
-			(this.parentNode.className == "fapp_client_box") ? this.parentNode.className = "fapp_client_box_selected" : this.parentNode.className = "fapp_client_box";
+			var class_parts = this.parentNode.className.split(" ");
+			(class_parts[0] == "fapp_client_box") ? this.parentNode.className = "fapp_client_selected " + class_parts[1] : this.parentNode.className = "fapp_client_box " + class_parts[1];
+			set_selected_num();
 		});
 	}
 	
 	//Selection tools - uses the same cat_links variable above!! Select all and none
 	document.getElementById("fapp_client_all").addEventListener('click',function(){
 		for(i = 0;i < n;i++){
-			cat_links[i].parentNode.className = "fapp_client_box_selected";
+			var class_parts = cat_links[i].parentNode.className.split(" ");
+			(class_parts[1] != "fapp_client_hidden") ? cat_links[i].parentNode.className = "fapp_client_selected " + class_parts[1] : null;
 		}
+		set_selected_num();
 	});
+	
 	document.getElementById("fapp_client_none").addEventListener('click',function(){
 		for(i = 0;i < n;i++){
-			cat_links[i].parentNode.className = "fapp_client_box";
+			var class_parts = cat_links[i].parentNode.className.split(" ");
+			(class_parts[1] != "fapp_client_hidden") ? cat_links[i].parentNode.className = "fapp_client_box " + class_parts[1] : null;
+		}
+		set_selected_num();
+	});
+	
+	//Search bar
+	var search_event = new CustomEvent("search");
+	document.getElementById("fapp_search_input_field").addEventListener('search',function(){
+		var n = json_arr.length;
+		var strFind = this.value.toLowerCase();
+		for(i = 0;i < n;i++){
+			var spec_node = document.getElementById(json_arr[i].id);
+			var ndclass = spec_node.className.split(" ");
+			if(strFind == ""){//Remove hidden from all
+				spec_node.className = ndclass[0] + " fapp_client_normal";
+			}else{
+				if(json_arr[i].name.toLowerCase().indexOf(strFind) != -1){
+					spec_node.className = ndclass[0] + " fapp_client_normal"; 
+				}else{			
+					spec_node.className = ndclass[0] + " fapp_client_hidden";
+				}
+			}
+		}
+		
+	});
+	
+	//Search bar tracking changes
+	setInterval(function(){
+		var bar = document.getElementById("fapp_search_input_field");
+		if(bar.value != prev_search){
+			bar.dispatchEvent(search_event);
+			prev_search = bar.value;
+		}
+	}, 200);
+	
+	//Keyboard shortcuts for search bar
+	document.getElementById("fapp_search_input_field").addEventListener('keydown',function(e){
+		switch(e.keyCode){
+			case 13: //Enter
+				var selected = get_search_results().selected;var n = get_search_results().n;var class_parts = get_search_results().class_parts;
+				if(class_parts[1] == "fapp_client_normal"){
+					selected[sel_index].className = class_parts[0] + " fapp_client_highlighted"; 
+				}else{//If already highlighted
+					(class_parts[0] == "fapp_client_box") ? selected[sel_index].className = "fapp_client_selected " + class_parts[1] : selected[sel_index].className = "fapp_client_box " + class_parts[1];
+				}
+				set_selected_num();
+				break;
+			case 38: //Up arrow
+				var selected = get_search_results().selected;var n = get_search_results().n;var class_parts = get_search_results().class_parts;
+				if(sel_index > 0 && selected[sel_index].className.split(" ")[1] == "fapp_client_highlighted"){
+					selected[sel_index].className = class_parts[0] + " fapp_client_normal";
+					sel_index -= 1;
+					class_parts = selected[sel_index].className.split(" ");
+					selected[sel_index].className = class_parts[0] + " fapp_client_highlighted";
+				}
+				break;
+			case 40: //Down arrow
+				var selected = get_search_results().selected;var n = get_search_results().n;var class_parts = get_search_results().class_parts;
+				if(sel_index < n - 1  && selected[sel_index].className.split(" ")[1] == "fapp_client_highlighted"){
+					selected[sel_index].className = class_parts[0] + " fapp_client_normal";
+					sel_index += 1;
+					class_parts = selected[sel_index].className.split(" ");
+					selected[sel_index].className = class_parts[0] + " fapp_client_highlighted";
+				}
+				break;
+			default:
+				sel_index = 0;
 		}
 	});
 }
@@ -190,11 +279,6 @@ function is_session_active(){
 		}
 	});
 }
-
-//Event listeners
-chrome.extension.onMessage.addListener(function(message, sender, sendResponse){
-	alert("Aquí oyendo!");
-});
 
 //Main! - It goes check_page() >> is_session_active() >> build_menu()
 check_page();
